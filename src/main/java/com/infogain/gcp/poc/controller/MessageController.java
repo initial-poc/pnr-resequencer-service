e8ccced43ec9963754d94c2f19df79845ca8b1ab
gc-user <aarfi.miracle@gmail.com>
2021-04-29 16:30:58 +0530

    code changes for service response

--- src/main/java/com/infogain/gcp/poc/controller/MessageController.java
+++ src/main/java/com/infogain/gcp/poc/controller/MessageController.java
@@ -1,11 +1,14 @@
 package com.infogain.gcp.poc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infogain.gcp.poc.domainmodel.PNRModel;
+import com.infogain.gcp.poc.domainmodel.ServiceResponse;
 import com.infogain.gcp.poc.service.PNRSequencingService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,9 +23,9 @@ public class MessageController {
 	private final PNRSequencingService pnrSequencingService;
 
 	@PostMapping("/pnrs")
-	public String processMessge(@RequestBody PNRModel message) {
-		log.info("Got the message in controller {}",message);
-	 
-		return	pnrSequencingService.processPNR(message);
-}
+	public ResponseEntity<ServiceResponse> processMessge(@RequestBody PNRModel message) {
+		log.info("Got the message in controller {}", message);
+		
+		return new ResponseEntity<ServiceResponse>(new ServiceResponse(pnrSequencingService.processPNR(message)), HttpStatus.CREATED);
+	}
 }
