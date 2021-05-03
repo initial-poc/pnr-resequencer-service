4a3277187d38eed9589df6fd70435943da54b5b2
initial-poc <gupta.hemanshu@gmail.com>
2021-05-03 17:21:29 +0530

    Added flux in release strategy

--- src/main/java/com/infogain/gcp/poc/service/PNRSequencingService.java
+++ src/main/java/com/infogain/gcp/poc/service/PNRSequencingService.java
@@ -12,6 +12,7 @@ import com.infogain.gcp.poc.util.RecordStatus;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.ConnectableFlux;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +22,6 @@ public class PNRSequencingService {
     private final PNRMessageGroupStore messageGroupStore;
     private final MessagePublisher messagePublisher;
 
-
     private final ReleaseStrategyService releaseStrategyService;
 
     public String processPNR(PNRModel pnrModel) {
@@ -29,36 +29,26 @@ public class PNRSequencingService {
         PNREntity pnrEntity = messageGroupStore.getMessageById(pnrModel);
         if (pnrEntity == null || pnrEntity.getStatus().equals(RecordStatus.FAILED.getStatusCode())) {
             pnrEntity = messageGroupStore.addMessage(pnrModel);
-            List<PNREntity> toReleaseMessage = releaseStrategyService.release(pnrEntity);
-            publishMessage(toReleaseMessage);
-            if(pnrEntity.getPnrid().startsWith("STK")) {
-                log.info("received STK record {}", pnrEntity.getPnrid());
-                pnrEntity.setStatus(RecordStatus.IN_PROGRESS.getStatusCode());
-            } else if(pnrEntity.getPnrid().startsWith("ERR")) {
-                log.info("received ERR record {}", pnrEntity.getPnrid());
-                pnrEntity.setStatus(RecordStatus.FAILED.getStatusCode());
-                throw new IllegalStateException("Failed ERR record..." + pnrEntity.getPnrid());
-            } else {
-                pnrEntity.setStatus(RecordStatus.RELEASED.getStatusCode());
-            }
+
+            ConnectableFlux<Object> flux = releaseStrategyService.release(pnrEntity);
+            flux.subscribe(entity-> {
+                publishMessage((PNREntity) entity);
+            });
+            flux.connect();
+            // this below commented line can only happen once release strategy has finished work...
+            // so we can't have it here below.
+            //pnrEntity.setStatus(RecordStatus.RELEASED.getStatusCode());
         } else {
             log.info("record is already in db and its status is {}", pnrEntity.getStatus());
         }
-
         log.info("process completed");
         return RecordStatus.getStatusMessage(pnrEntity.getStatus()).toString();
     }
 
-    private void publishMessage(List<PNREntity> toReleaseMessage) {
-
-        log.info("Going to update status for messages {}", toReleaseMessage);
-        toReleaseMessage.stream().forEach(entity -> {
-            messageGroupStore.updateStatus(entity, RecordStatus.RELEASED.getStatusCode());
-            messagePublisher.publishMessage(entity);
-            messageGroupStore.updateStatus(entity, RecordStatus.COMPLETED.getStatusCode());
-
-        });
-
+    private void publishMessage(PNREntity entity) {
+        messageGroupStore.updateStatus((PNREntity) entity, RecordStatus.RELEASED.getStatusCode());
+        messagePublisher.publishMessage((PNREntity)entity);
+        messageGroupStore.updateStatus((PNREntity)entity, RecordStatus.COMPLETED.getStatusCode());
     }
 
 }
--- src/main/java/com/infogain/gcp/poc/service/ReleaseStrategyService.java
+++ src/main/java/com/infogain/gcp/poc/service/ReleaseStrategyService.java
@@ -14,6 +14,8 @@ import com.infogain.gcp.poc.util.RecordStatus;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
 
 @Slf4j
 @Service
@@ -23,13 +25,14 @@ public class ReleaseStrategyService {
     private final GroupMessageStoreRepository msgGrpStoreRepository;
 
     @SuppressWarnings("all")
-    public List<PNREntity> release(PNREntity pnrEntity) {
+    public ConnectableFlux<Object> release(PNREntity pnrEntity) {
+
+        ConnectableFlux<Object> flux = Flux.create(fluxSink->{
         log.info("Getting all the messages from the table by pnr id");
         Optional<List<PNREntity>> pnrEntityList = msgGrpStoreRepository.findByPnrid(pnrEntity.getPnrid());
 
         List<PNREntity> pnrList = pnrEntityList.get();
         log.info("Messages are {}", pnrList);
-        List<PNREntity> returnList = new ArrayList<PNREntity>();
 
         Map<Integer, Boolean> seqReleasedStatusMap = pnrList.stream()
                 .collect(Collectors.toMap(PNREntity::getMessageseq, x -> x.getStatus().equals(RecordStatus.RELEASED.getStatusCode()) ? true : false));
@@ -39,7 +42,7 @@ public class ReleaseStrategyService {
         if (Optional.ofNullable(seqReleasedStatusMap.get(1)).isPresent()) {
             seqReleasedStatusMap.put(1, true);
             if (!seqPNREntityMap.get(1).getStatus().equals(RecordStatus.RELEASED.getStatusCode())) {
-                returnList.add(seqPNREntityMap.get(1));
+                fluxSink.next(seqPNREntityMap.get(1));
             }
         }
 
@@ -52,18 +55,20 @@ public class ReleaseStrategyService {
                 filter(x -> !seqPNREntityMap.get(x.getMessageseq()).getStatus().equals(RecordStatus.RELEASED.getStatusCode()))
                 .forEach(x -> {
                     seqReleasedStatusMap.put(x.getMessageseq(), true);
-                    returnList.add(x);
+                    fluxSink.next(x);
                 });
 
-        log.info("returning the list {}", returnList);
-        return returnList;
+        }).publish();
+        return flux;
     }
 
-    public static List<PNREntity> releaseTest(List<PNREntity> pnrList) {
+    public static ConnectableFlux<Object> releaseTest(List<PNREntity> pnrList) {
+
+        ConnectableFlux<Object> flux = Flux.create(fluxSink->{
         log.info("Getting all the messages from the table by pnr id");
         //	pnrList.add(pnrEntity);
         log.info("Messages are {}", pnrList);
-        List<PNREntity> returnList = new ArrayList<PNREntity>();
+//        List<PNREntity> returnList = new ArrayList<PNREntity>();
 
         Map<Integer, Boolean> seqReleasedStatusMap = pnrList.stream()
                 .collect(Collectors.toMap(PNREntity::getMessageseq, x -> x.getStatus().equals(RecordStatus.RELEASED.getStatusCode()) ? true : false));
@@ -73,7 +78,7 @@ public class ReleaseStrategyService {
         if (Optional.ofNullable(seqReleasedStatusMap.get(1)).isPresent()) {
             seqReleasedStatusMap.put(1, true);
             if (!seqPNREntityMap.get(1).getStatus().equals(RecordStatus.RELEASED.getStatusCode())) {
-                returnList.add(seqPNREntityMap.get(1));
+                fluxSink.next(seqPNREntityMap.get(1));
             }
         }
 
@@ -86,76 +91,24 @@ public class ReleaseStrategyService {
                 filter(x -> !seqPNREntityMap.get(x.getMessageseq()).getStatus().equals(RecordStatus.RELEASED.getStatusCode()))
                 .forEach(x -> {
                     seqReleasedStatusMap.put(x.getMessageseq(), true);
-                    returnList.add(x);
+                    fluxSink.next(x);
                 });
 
-        log.info("returning the list {}", returnList);
-        return returnList;
+        }).publish();
+        return flux;
     }
 
     public static void main(String[] args) {
         List<PNREntity> returnList = new ArrayList<PNREntity>();
         returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(1).status(RecordStatus.RELEASED.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(3).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-//		returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(1).status(RecordStatus.RELEASED.getStatusCode()).build());
-//		returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(2).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-		/*returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(9).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());*/
-        releaseTest(returnList).stream().forEach(System.out::println);
-        returnList = new ArrayList<PNREntity>();
-
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(1).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
         returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(2).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(7).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(8).status(RecordStatus.RELEASED.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(9).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        releaseTest(returnList).stream().forEach(System.out::println);
-        returnList = new ArrayList<PNREntity>();
-
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(1).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(2).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(3).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(7).status(RecordStatus.RELEASED.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(8).status(RecordStatus.RELEASED.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(9).status(RecordStatus.RELEASED.getStatusCode()).build());
-        releaseTest(returnList).stream().forEach(System.out::println);
-        returnList = new ArrayList<PNREntity>();
-
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(1).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(2).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(3).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(8).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(7).status(RecordStatus.RELEASED.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(9).status(RecordStatus.RELEASED.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(10).status(RecordStatus.RELEASED.getStatusCode()).build());
-
-        releaseTest(returnList).stream().forEach(System.out::println);
-        returnList = new ArrayList<PNREntity>();
-
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(1).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(2).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(3).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(8).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(9).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(7).status(RecordStatus.RELEASED.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(10).status(RecordStatus.RELEASED.getStatusCode()).build());
-
-        releaseTest(returnList).stream().forEach(System.out::println);
-        returnList = new ArrayList<PNREntity>();
-
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(1).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(2).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(3).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(4).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(5).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(6).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(7).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(8).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(9).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
-        returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(10).status(RecordStatus.RELEASED.getStatusCode()).build());
-
-        releaseTest(returnList).stream().forEach(System.out::println);
-        returnList = new ArrayList<PNREntity>();
-
+		returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(3).status(RecordStatus.RELEASED.getStatusCode()).build());
+		returnList.add(PNREntity.builder().pnrid("PNR123").messageseq(4).status(RecordStatus.IN_PROGRESS.getStatusCode()).build());
+        ConnectableFlux<Object> flux = releaseTest(returnList);
+        flux.subscribe(x-> {
+            System.out.println((PNREntity)x);
+        });
+        flux.connect();
     }
 
 }
