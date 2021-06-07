dc30c45a78c9e545fe684f8e3fb79504387714f7
SG0310720 <biswabhusan.swain.ctr@sabre.com>
2021-06-07 16:36:30 +0530

    performance testing[featching 10000 records from db]

--- src/main/java/com/infogain/gcp/poc/poller/service/OutboxRecordProcessorService.java
+++ src/main/java/com/infogain/gcp/poc/poller/service/OutboxRecordProcessorService.java
@@ -18,49 +18,50 @@ import java.util.List;
 @Slf4j
 public class OutboxRecordProcessorService {
 
-	private final APIGatewayService outboxStatusService;
-	private final SpannerOutboxRepository spannerOutboxRepository;
-	private final String ip;
+    private final APIGatewayService outboxStatusService;
+    private final SpannerOutboxRepository spannerOutboxRepository;
+    private final String ip;
 
-	//@Value(value = "${limit}")
-	private int recordLimit=10;
+    //@Value(value = "${limit}")
+    private static int recordLimit = 10000;
 
-	private static final String OUTBOX_SQL = "SELECT * FROM OUTBOX WHERE STATUS =0 order by updated desc";
-	private static final String OUTBOX_FAILED_RECORD_SQL =
-			"SELECT * FROM OUTBOX WHERE STATUS =3 and retry_count<=3";
-	private static final String OUTBOX_STUCK_RECORD_SQL =
-			"SELECT * FROM OUTBOX WHERE STATUS =1 and TIMESTAMP_DIFF(CURRENT_TIMESTAMP,updated, MINUTE)>5";
+    private static final String OUTBOX_SQL = "SELECT * FROM OUTBOX WHERE STATUS =0 order by updated desc LIMIT %s";
+    private static final String OUTBOX_FAILED_RECORD_SQL =
+            "SELECT * FROM OUTBOX WHERE STATUS =3 and retry_count<=3";
+    private static final String OUTBOX_STUCK_RECORD_SQL =
+            "SELECT * FROM OUTBOX WHERE STATUS =1 and TIMESTAMP_DIFF(CURRENT_TIMESTAMP,updated, MINUTE)>5";
 
-	@Autowired
-	@SneakyThrows
-	public OutboxRecordProcessorService(APIGatewayService outboxStatusService, SpannerOutboxRepository spannerOutboxRepository) {
-		this.outboxStatusService = outboxStatusService;
-		this.spannerOutboxRepository = spannerOutboxRepository;
-		ip = InetAddress.getLocalHost().getHostAddress();
-	}
+    @Autowired
+    @SneakyThrows
+    public OutboxRecordProcessorService(APIGatewayService outboxStatusService, SpannerOutboxRepository spannerOutboxRepository) {
+        this.outboxStatusService = outboxStatusService;
+        this.spannerOutboxRepository = spannerOutboxRepository;
+        ip = InetAddress.getLocalHost().getHostAddress();
+    }
 
-	public void processRecords() {
-		doProcess(getRecord(OUTBOX_SQL));
-	}
+    public void processRecords() {
+        doProcess(getRecord(OUTBOX_SQL));
+    }
 
-	public void doProcess(List<OutboxEntity> recordToProcess) {
-		log.info("total record -> {} to process by application->  {}", recordToProcess.size(), ip);
-		log.info("RECORD {}", recordToProcess);
-		process(recordToProcess);
-	}
+    public void doProcess(List<OutboxEntity> recordToProcess) {
+        log.info("total record -> {} to process by application->  {}", recordToProcess.size(), ip);
+        log.info("RECORD {}", recordToProcess);
+        process(recordToProcess);
+    }
 
-	private List<OutboxEntity> getRecord(String sql) {
-		log.info("Getting record to process by application->  {}", ip);
-		Stopwatch stopWatch = Stopwatch.createStarted();
-		SpannerOperations spannerTemplate = spannerOutboxRepository.getSpannerTemplate();
-		List<OutboxEntity> recordToProcess = spannerTemplate.query(OutboxEntity.class,
-				Statement.of(String.format(sql, recordLimit)), null);
-		stopWatch.stop();
-		log.info("Total time taken to fetch the records {}", stopWatch);
-		return recordToProcess;
+    private List<OutboxEntity> getRecord(String sql) {
+        log.info("Getting record to process by application->  {}", ip);
+        Stopwatch stopWatch = Stopwatch.createStarted();
+        SpannerOperations spannerTemplate = spannerOutboxRepository.getSpannerTemplate();
+        List<OutboxEntity> recordToProcess = spannerTemplate.query(OutboxEntity.class,
+                Statement.of(String.format(sql, recordLimit)), null);
+        stopWatch.stop();
+        log.info("Total time taken to fetch the records {}", stopWatch);
+        return recordToProcess;
 
-	}
-	private void process(List<OutboxEntity> outboxEntities) {
-		outboxEntities.stream().forEach(outboxStatusService::processRecord);
-	}
+    }
+
+    private void process(List<OutboxEntity> outboxEntities) {
+        outboxEntities.stream().forEach(outboxStatusService::processRecord);
+    }
 }
