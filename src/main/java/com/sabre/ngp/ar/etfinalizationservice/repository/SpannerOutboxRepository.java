072f0a76419e3e91833c8a5cb0cb6eca97ebe2c2
initial-poc <chepuri.uk2021@gmail.com>
2021-07-14 17:04:50 +0530

    limit added into the sql.

--- src/main/java/com/sabre/ngp/ar/etfinalizationservice/repository/SpannerOutboxRepository.java
+++ src/main/java/com/sabre/ngp/ar/etfinalizationservice/repository/SpannerOutboxRepository.java
@@ -19,7 +19,7 @@ public class SpannerOutboxRepository {
 
 
     private final DatabaseClient databaseClient;
-    private static final String OUTBOX_SQL = "select * from outbox where created in (select  min(created) from OUTBOX  where status in (0,3) group by  locator) ";
+    private static final String OUTBOX_SQL = "select * from outbox where created in (select  min(created) from OUTBOX  where status in (0,3) group by  locator limit 500) ";
 
     public List<OutboxEntity> getRecords() {
         ResultSet rs = databaseClient.singleUse().executeQuery(Statement.of(OUTBOX_SQL));
