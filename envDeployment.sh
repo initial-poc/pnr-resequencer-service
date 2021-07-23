5ab13f76d7d2473304bd0d464684e01083fe7a9d
initial-poc <chepuri.uk2021@gmail.com>
2021-07-23 13:38:11 +0530

    sh file updated

--- envDeployment.sh
+++ envDeployment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 mvn clean package
 docker build . -t gcr.io/sab-ors-poc-sbx-01-9096/pnr-resequencer-service
 docker push gcr.io/sab-ors-poc-sbx-01-9096/pnr-resequencer-service:latest
