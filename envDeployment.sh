f8b6cc6d839b3d8ad69b9b6daa83dce55047567d
initial-poc <chepuri.uk2021@gmail.com>
2021-07-23 13:29:11 +0530

    docker file updated

---
+++ envDeployment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mvn clean package
+docker build . -t gcr.io/sab-ors-poc-sbx-01-9096/pnr-resequencer-service
+docker push gcr.io/sab-ors-poc-sbx-01-9096/pnr-resequencer-service:latest
+kubectl apply -f deployment.yaml
