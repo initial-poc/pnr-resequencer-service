7708c8079a0113e0af17f0ece16ca86a3017a19a
initial-poc <chepuri.uk2021@gmail.com>
2021-07-27 15:44:24 +0530

    creating object of publisher

--- src/main/java/com/sabre/ngp/ar/etfinalizationservice/component/MessagePublisher.java
+++ src/main/java/com/sabre/ngp/ar/etfinalizationservice/component/MessagePublisher.java
@@ -2,6 +2,7 @@ package com.sabre.ngp.ar.etfinalizationservice.component;
 
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutures;
+import com.google.api.gax.batching.BatchingSettings;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.gson.Gson;
 import com.google.protobuf.ByteString;
@@ -11,7 +12,10 @@ import com.sabre.ngp.ar.etfinalizationservice.entity.OutboxEntity;
 import com.sabre.ngp.ar.etfinalizationservice.util.PublisherUtility;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.threeten.bp.Duration;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,7 +28,9 @@ import java.util.concurrent.TimeUnit;
 public class MessagePublisher {
     private final Gson gson;
 private final PublisherUtility publisherUtility;
-private final Publisher pubsubPublisher;
+//private final Publisher pubsubPublisher;
+    @Value("${pubsubBatchSize}")
+    private long pubsubBatchSize;
 
     public void publishMessage(List<OutboxEntity> entities) throws InterruptedException, IOException {
 
@@ -35,14 +41,13 @@ private final Publisher pubsubPublisher;
     private void sendMessage(String topicName,List<OutboxEntity> entities)  {
         List<ApiFuture<String>> messageIdFutures = new ArrayList<>();
 
-        Publisher publisher =null;
+        Publisher pubsubPublisher=getPublisher();
         try{
 
         for(OutboxEntity entity: entities) {
             PNRModel model = entity.buildEntity();
             PubsubMessage pubsubMessage = getPubsubMessage(model);
             ApiFuture<String> future = pubsubPublisher.publish(pubsubMessage);
-            log.info("message added into publisher");
             messageIdFutures.add(future);
         }
             } catch (Exception ex) {
@@ -51,11 +56,10 @@ private final Publisher pubsubPublisher;
             } finally {
                 try {
                     List<String> messageIds = ApiFutures.allAsList(messageIdFutures).get();
-
                    log.info("Published {} messages with batch settings.",messageIds.size());
-                   if(publisher!=null) {
-                       publisher.shutdown();
-                       publisher.awaitTermination(1, TimeUnit.MINUTES);
+                   if(pubsubPublisher!=null) {
+                       pubsubPublisher.shutdown();
+                       pubsubPublisher.awaitTermination(1, TimeUnit.MINUTES);
                    }
                 } catch (Exception ex) {
                     log.info("Exception occurred while shutdown the pubsub {}", ex.getMessage());
@@ -84,5 +88,38 @@ private final Publisher pubsubPublisher;
             log.error("Exception while converting record to json {}", e);
         }
         return result;
+    }
+
+
+    public BatchingSettings PubSubBatchConfiguration(){
+        long requestBytesThreshold = 2000000L; // default : 1 byte
+
+        Duration publishDelayThreshold = Duration.ofMillis(100); // default : 1 ms
+
+        // Publish request get triggered based on request size, messages count & time since last
+        // publish, whichever condition is met first.
+        return
+                BatchingSettings.newBuilder()
+                        .setElementCountThreshold(pubsubBatchSize)
+                        .setRequestByteThreshold(requestBytesThreshold)
+                        .setDelayThreshold(publishDelayThreshold)
+                        .build();
+
+
+    }
+
+
+    private Publisher getPublisher()  {
+        String topicName="projects/sab-ors-poc-sbx-01-9096/topics/itinerary-topic";
+        Publisher publisher=null;
+        try {
+              publisher = Publisher.newBuilder(topicName)
+                    //.setEndpoint("us-central1-pubsub.googleapis.com:443")
+                    .setBatchingSettings(PubSubBatchConfiguration())
+                    .build();
+        }catch(Exception ex){
+            log.error("Got error while creating publisher {}",ex.getMessage());
+        }
+        return publisher;
     }
 }
