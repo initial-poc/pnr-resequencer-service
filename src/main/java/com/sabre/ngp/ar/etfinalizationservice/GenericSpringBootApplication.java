1cb50004dc29374a6042770d82e5435df215d137
initial-poc <chepuri.uk2021@gmail.com>
2021-08-04 18:00:58 +0530

    test code remvoed.

--- src/main/java/com/sabre/ngp/ar/etfinalizationservice/GenericSpringBootApplication.java
+++ src/main/java/com/sabre/ngp/ar/etfinalizationservice/GenericSpringBootApplication.java
@@ -1,19 +1,10 @@
 package com.sabre.ngp.ar.etfinalizationservice;
 
-import com.google.common.base.Stopwatch;
-import com.google.common.collect.Lists;
 import com.sabre.ngp.ar.etfinalizationservice.component.OutboxPollerExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.util.List;
-import java.util.SortedSet;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class GenericSpringBootApplication implements CommandLineRunner {
@@ -21,10 +12,7 @@ public class GenericSpringBootApplication implements CommandLineRunner {
     @Autowired
     private OutboxPollerExecutor executor;
     public static void main(String[] args) throws Exception{
-        Stopwatch s= Stopwatch.createStarted();
-        TimeUnit.MINUTES.sleep(1);
-        System.out.println(s.stop());
-   //   SpringApplication.run(GenericSpringBootApplication.class, args);
+   SpringApplication.run(GenericSpringBootApplication.class, args);
     }
 
     @Override
