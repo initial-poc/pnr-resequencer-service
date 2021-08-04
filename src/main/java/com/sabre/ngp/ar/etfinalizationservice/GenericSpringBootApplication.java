2250d4159309b99a6e1bdafe535a5d3488882445
initial-poc <chepuri.uk2021@gmail.com>
2021-08-04 18:23:24 +0530

    remvoed the test

--- src/main/java/com/sabre/ngp/ar/etfinalizationservice/GenericSpringBootApplication.java
+++ src/main/java/com/sabre/ngp/ar/etfinalizationservice/GenericSpringBootApplication.java
@@ -6,30 +6,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 @SpringBootApplication
 public class GenericSpringBootApplication implements CommandLineRunner {
 
     @Autowired
     private OutboxPollerExecutor executor;
     public static void main(String[] args) throws Exception{
-  // SpringApplication.run(GenericSpringBootApplication.class, args);
-        {
-            ThreadPoolExecutor threadPoolExecutor =   new ThreadPoolExecutor(5, 5,
-                    0L, TimeUnit.MILLISECONDS,
-                    new LinkedBlockingQueue<Runnable>(10));
-            threadPoolExecutor.submit(() -> {
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-            System.out.println(threadPoolExecutor.getActiveCount());
-        }
+     SpringApplication.run(GenericSpringBootApplication.class, args);
+
     }
 
     @Override
