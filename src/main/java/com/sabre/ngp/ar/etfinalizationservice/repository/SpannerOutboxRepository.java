eb5721ce292ef23aa3f462c116c05e9437287487
initial-poc <chepuri.uk2021@gmail.com>
2021-08-06 13:47:23 +0530

    query limit fixed to 200

--- src/main/java/com/sabre/ngp/ar/etfinalizationservice/repository/SpannerOutboxRepository.java
+++ src/main/java/com/sabre/ngp/ar/etfinalizationservice/repository/SpannerOutboxRepository.java
@@ -29,35 +29,36 @@ public class SpannerOutboxRepository {
     private int pubsubBatchSize;
 
     private final DatabaseClient databaseClient;
-    private static final String OUTBOX_SQL = "select  * from OUTBOX  where status =0 order by created limit %s";
+    private static final String OUTBOX_SQL = "select  locator,version,payload from OUTBOX  where status status in (0,3) order by created limit %s";
 
     public List<OutboxEntity> getRecords(Map<String,String> metaData) {
         Stopwatch stopwatch= Stopwatch.createStarted();
-                 log.info("remainingCapacity {}",threadPoolExecutor.getQueue().remainingCapacity());
+                /* log.info("remainingCapacity {}",threadPoolExecutor.getQueue().remainingCapacity());
                  if(threadPoolExecutor.getQueue().remainingCapacity()!=0){
                      queryLimit = threadPoolExecutor.getQueue().remainingCapacity();
                  }else{
                      log.info("in else");
                      queryLimit=0;
-                 }
+                 }*/
 
-        log.info("Going to perform query with limit {}",queryLimit);
+       // log.info("Going to perform query with limit {}",queryLimit);
+
 
         Stopwatch queryStopWatch= Stopwatch.createStarted();
-        ResultSet rs = databaseClient.singleUse().executeQuery(Statement.of(String.format(OUTBOX_SQL, queryLimit)));
+        ResultSet rs = databaseClient.singleUse().executeQuery(Statement.of(String.format(OUTBOX_SQL, "300")));
         queryStopWatch=queryStopWatch.stop();
         metaData.put("query_time",queryStopWatch.toString());
         List<OutboxEntity> outboxEntities = Lists.newArrayList();
         while (rs.next()) {
             OutboxEntity entity = new OutboxEntity();
-            entity.setCreated(rs.getTimestamp("created"));
+           // entity.setCreated(rs.getTimestamp("created"));
             entity.setVersion(rs.getLong("version"));
             entity.setLocator(rs.getString("locator"));
             entity.setPayload(rs.getString("payload"));
-            if (!rs.isNull("parent_locator")) {
+         /*   if (!rs.isNull("parent_locator")) {
                 entity.setParentPnr(rs.getString("parent_locator"));
             }
-            entity.setStatus(rs.getLong("status"));
+            entity.setStatus(rs.getLong("status"));*/
             outboxEntities.add(entity);
         }
         stopwatch=    stopwatch.stop();
