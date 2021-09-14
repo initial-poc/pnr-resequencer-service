25455560fb8d362225c4f36fd51f85cfe6a0d629
initial-poc <chepuri.uk2021@gmail.com>
2021-09-14 16:31:44 +0530

    gcp project name changed

--- envDeployment.sh
+++ envDeployment.sh
@@ -2,6 +2,6 @@
 kubectl delete deployment pnr-resequencer-service-deployment
 git pull
 mvn clean package
-docker build . -t gcr.io/sab-ors-poc-sbx-01-9096/pnr-resequencer-service
-docker push gcr.io/sab-ors-poc-sbx-01-9096/pnr-resequencer-service:latest
+docker build . -t gcr.io/sab-order-service-sbx-7006/pnr-resequencer-service
+docker push gcr.io/sab-order-service-sbx-7006/pnr-resequencer-service:latest
 kubectl apply -f deployment.yaml
